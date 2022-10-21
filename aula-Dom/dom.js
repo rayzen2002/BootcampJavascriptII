70aedd33079c27ca2bb0b8c5ae3260ccf09a3b6a
Emanuel <emanuelzsmj@gmail.com>
2022-10-21 17:55:00 -0300

    Aula-DOM

---
+++ aula-Dom/dom.js
@@ -0,0 +1,13 @@
+function show() {
+  const contactList = document.getElementById('contact-list')
+  console.log(contactList)
+
+  const listElements = document.getElementsByTagName('li')
+  console.log(listElements)
+
+  const contactInputs = document.getElementsByClassName('contact-input')
+  console.log(contactInputs)
+
+  const contacts = document.querySelectorAll('#contact-list > li > label')
+  console.log(contacts)
+}
