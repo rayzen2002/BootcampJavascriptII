2676da986908aae82e14e847fe24bedc28e9e9cf
Emanuel <emanuelzsmj@gmail.com>
2023-03-23 20:49:09 -0300

    Eventos

---
+++ eventos/eventos.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+  console.log('Entrada do mouse')
+}
+
+function onChange() {
+  console.log('Mudando valor')
+}
+
+function onFocus() {
+  console.log('Focando elemento')
+}
+
+function onBlur() {
+  console.log('Desfocando elemento')
+}
+
+function onClick() {
+  console.log('Botao Clicado')
+}
